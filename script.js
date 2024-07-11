0f202e13639a53a4f8358758c4dfffaebf6a6903
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-11 16:36:24 -0300

    Add playGame function to run the game five times
    The function is called once, and runs playRound five
    times while keeping track of both computer's and player's
    scores. Adds a function called showResults, that is declared
    and ran inside of playGame, and it shows who the winner is
    after the rounds are run. Variables to keep track of scores
    have been moved to inside of playGame.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let playerScore = 0;
-let computerScore = 0;
-
 function getPlayerChoice(){
   let playerChoice= prompt("Pedra, papel ou tesoura?", "qual vai ser?")
   switch (playerChoice.toLowerCase()){
@@ -21,49 +18,75 @@ function getComputerChoice(){
   }
 }
 
+function playGame(){
+    let playerScore = 0;
+    let computerScore = 0;
 
-playRound(getPlayerChoice, getComputerChoice);
+    playRound(getPlayerChoice, getComputerChoice);
+    playRound(getPlayerChoice, getComputerChoice);
+    playRound(getPlayerChoice, getComputerChoice);
+    playRound(getPlayerChoice, getComputerChoice);
+    playRound(getPlayerChoice, getComputerChoice);
+    showResult();
+    
+    function showResult(){
+        if (playerScore > computerScore){
+            console.log("voce ganhou! o placar ficou: " + playerScore + " a " + computerScore);
+        } else if(computerScore > playerScore){
+            console.log("voce perdeu :( o placar ficou: " + computerScore + " a " + playerScore);
+        } else {
+            console.log("it's a tie!")
+        }
+    }
 
-function playRound(playerChoice, computerChoice){
-    const roundComputerChoice = computerChoice();
-    const roundPlayerChoice = playerChoice();
+    function playRound(playerChoice, computerChoice){
+        const roundComputerChoice = computerChoice();
+        const roundPlayerChoice = playerChoice();
+    
+        console.log("voce jogou: " + roundPlayerChoice);
+        console.log("o computador jogou: " + roundComputerChoice);
+    
+        switch (roundPlayerChoice){
+        case "rock":
+            if (roundComputerChoice === "rock"){
+            console.log("it's a tie!");
+            } else if (roundComputerChoice === "paper"){
+            console.log("computer wins!");
+            computerScore += 1;
+            } else if (roundComputerChoice === "scissor") {
+            console.log("player wins!");
+            playerScore += 1;
+            }
+            break;
+        case "paper":
+            if (roundComputerChoice === "rock"){
+            console.log("player wins!");
+            playerScore += 1;
+            } else if (roundComputerChoice === "paper"){
+            console.log("it's a tie!");
+            } else if (roundComputerChoice === "scissor") {
+            console.log("computer wins!");
+            computerScore += 1;
+            }
+            break;
+        case "scissor":  
+            if (roundComputerChoice === "rock"){
+            console.log("computer wins!");
+            computerScore += 1;
+            } else if (roundComputerChoice === "paper"){
+            console.log("player wins!");
+            playerScore += 1;
+            } else if (roundComputerChoice === "scissor") {
+            console.log("it's a tie!");
+            }
+            break;
+            }
+    }
+    
 
-    console.log("voce jogou: " + roundPlayerChoice);
-    console.log("o computador jogou: " + roundComputerChoice);
-
-    switch (roundPlayerChoice){
-    case "rock":
-        if (roundComputerChoice === "rock"){
-        console.log("it's a tie!");
-        } else if (roundComputerChoice === "paper"){
-        console.log("computer wins!");
-        computerScore += 1;
-        } else if (roundComputerChoice === "scissor") {
-        console.log("player wins!");
-        playerScore += 1;
-        }
-        break;
-    case "paper":
-        if (roundComputerChoice === "rock"){
-        console.log("player wins!");
-        playerScore += 1;
-        } else if (roundComputerChoice === "paper"){
-        console.log("it's a tie!");
-        } else if (roundComputerChoice === "scissor") {
-        console.log("computer wins!");
-        computerScore += 1;
-        break;
-        }
-    case "scissor":  
-        if (roundComputerChoice === "rock"){
-        console.log("computer wins!");
-        computerScore += 1;
-        } else if (roundComputerChoice === "paper"){
-        console.log("player wins!");
-        playerScore += 1;
-        } else if (roundComputerChoice === "scissor") {
-        console.log("it's a tie!");
-        break;
-        }
-        }
 }
+
+playGame();
+
+
+
