11871445f4bef34812a3b9e1715c554ba55eafe0
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-18 15:39:49 -0300

    Add function to clear scores when match ends
    The function simply resets the scores. When
    attempting to play again, the victory/lose
    text also disappear. Additionally, minor fixes
    in labeling and structure on JS code were made
    to make it more readable.

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 const buttons = document.querySelector("ul");
 const divResults = document.querySelector(".results");
+const lastRoundChoices = document.querySelector(".lastRoundChoices");
+const lastRoundWinner = document.querySelector(".lastRoundWinner");
+const gameWinner = document.querySelector(".gameWinner")
+const scoresTracker = document.querySelector(".scoresTracker")
+
 let playerScore = 0;
 let computerScore = 0;
-let currentPlayerChoice;
-let lastRoundChoices = document.querySelector(".lastRoundChoices");
-let lastRoundWinner = document.querySelector(".lastRoundWinner");
-let gameWinner = document.querySelector(".gameWinner")
-let scoresTracker = document.querySelector(".scoresTracker")
+
+
+buttons.addEventListener('click', function (e){
+    let target = e.target.id;
+    let currentPlayerChoice;
+  
+    currentPlayerChoice = target;
+    playRound(currentPlayerChoice, getComputerChoice)  
+    showScores();
+    gameWinner.textContent = '';
+    if(playerScore > 2 || computerScore > 2){
+                showFinalResult();
+            }
+    
+});
+
 
 function showScores(){
     scoresTracker.textContent = "placar atual: player " + playerScore + " x " + computerScore + " computador."; 
 }
 
-buttons.addEventListener('click', function (e){
-    let target = e.target.id;
-    currentPlayerChoice = target;
-    playRound(currentPlayerChoice, getComputerChoice)  
-    showScores();
-    if(playerScore > 2 || computerScore > 2){
-                showFinalResult();
-            }
-});
 
 function getComputerChoice(){
   let randomizer = Math.floor(Math.random()*3);
- 
   switch (randomizer){
     case 0: return "rock";
     case 1: return "paper";
     case 2: return "scissor";
   }
+}
+
+function clearScores(){
+    playerScore = 0;
+    computerScore = 0;
 }
 
 function showFinalResult(){
@@ -42,8 +53,8 @@ function showFinalResult(){
     } else {
         console.log("it's a tie!");
     }
+    clearScores();
 }
-
 
 
 function playRound(playerChoice, computerChoice){
@@ -51,17 +62,15 @@ function playRound(playerChoice, computerChoice){
     const roundPlayerChoice = playerChoice;
     
     function showRoundChoices(){
-        //criar paragrafo
         lastRoundChoices.textContent = "voce jogou: " + roundPlayerChoice + ", o computador jogou: " +                             roundComputerChoice;
-        //append child
     }
 
-    function showWinner(winner){
+    function showRoundWinner(winner){
         //criar paragrafo
         lastRoundWinner.textContent = winner;
     }
 
-
+  
     showRoundChoices();
 
     console.log("voce jogou: " + roundPlayerChoice);
@@ -71,46 +80,47 @@ function playRound(playerChoice, computerChoice){
     case "rock":
         if (roundComputerChoice === "rock"){
         console.log("it's a tie!");
-        showWinner("it's a tie!")
+        showRoundWinner("it's a tie!")
         } else if (roundComputerChoice === "paper"){
         console.log("computer wins!");
-        showWinner("computer wins!")
+        showRoundWinner("computer wins!")
         computerScore += 1;
         } else if (roundComputerChoice === "scissor") {
         console.log("player wins!");
-        showWinner("player wins!")
+        showRoundWinner("player wins!")
         playerScore += 1;
         }
         break;
     case "paper":
         if (roundComputerChoice === "rock"){
         console.log("player wins!");
-        showWinner("player wins!");
+        showRoundWinner("player wins!");
         playerScore += 1;
         } else if (roundComputerChoice === "paper"){
         console.log("it's a tie!");
-        showWinner("it's a tie!");
+        showRoundWinner("it's a tie!");
         } else if (roundComputerChoice === "scissor") {
         console.log("computer wins!");
-        showWinner("computer wins!");
+        showRoundWinner("computer wins!");
         computerScore += 1;
         }
         break;
     case "scissor":  
         if (roundComputerChoice === "rock"){
         console.log("computer wins!");
-        showWinner("computer wins!");
+        showRoundWinner("computer wins!");
         computerScore += 1;
         } else if (roundComputerChoice === "paper"){
         console.log("player wins!");
-        showWinner("player wins!");
+        showRoundWinner("player wins!");
         playerScore += 1;
         } else if (roundComputerChoice === "scissor") {
         console.log("it's a tie!");
-        showWinner("it's a tie!");
+        showRoundWinner("it's a tie!");
         }
         break;
         }
 }
 
-
+//whats left: style manipulation: color, size of texts. reset game button?
+//delete UL choice buttons.
